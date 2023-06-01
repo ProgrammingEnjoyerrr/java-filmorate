d210658d6b7d285d38b468fa35f1af5fa082c878
Ruslan Frolov <r.frolov@1440.space>
2023-06-01 22:47:20 +0300

    test: add film controller tests

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTests.java
@@ -1,4 +1,81 @@
 package ru.yandex.practicum.filmorate.controller;
 
-public class FilmControllerTests {
+import org.junit.jupiter.api.*;
+import ru.yandex.practicum.filmorate.exception.FilmValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class FilmControllerTests {
+
+    private static FilmController filmController;
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    private static final int ANY_ID = 0;
+
+    @BeforeAll
+    private static void init() {
+        filmController = new FilmController();
+    }
+
+    @Test
+    @Order(1)
+    void testCreateFilmShouldBeOk() {
+        final Film film = new Film(ANY_ID, "nisi eiusmod", "adipisicing", LocalDate.parse("1967-03-25", FORMATTER), 100);
+
+        final Film created = filmController.create(film);
+
+        assertEquals(1, created.getId());
+        assertEquals(created.getName(), film.getName());
+        assertEquals(created.getDescription(), film.getDescription());
+        assertEquals(created.getReleaseDate(), film.getReleaseDate());
+        assertEquals(created.getDuration(), film.getDuration());
+    }
+
+    @Test
+    @Order(2)
+    void testUpdateFilmShouldBeOk() {
+        final Film film = new Film(1, "Film Updated", "New film update description", LocalDate.parse("1989-04-17", FORMATTER), 190);
+
+        final Film updated = filmController.update(film);
+
+        assertEquals(1, updated.getId());
+        assertEquals(updated.getName(), film.getName());
+        assertEquals(updated.getDescription(), film.getDescription());
+        assertEquals(updated.getReleaseDate(), film.getReleaseDate());
+        assertEquals(updated.getDuration(), film.getDuration());
+    }
+
+    @Test
+    @Order(3)
+    void testUpdateUnknownFilmShouldThrow() {
+        final Film film = new Film(9999, "Film Updated", "New film update description", LocalDate.parse("1989-04-17", FORMATTER), 190);
+
+        final FilmValidationException exception = assertThrows(FilmValidationException.class,
+                () -> filmController.update(film));
+        assertEquals("Попытка обновить фильм с несуществующим id 9999", exception.getMessage());
+    }
+
+    @Test
+    @Order(4)
+    void testGetAllFilmsShouldBeOk() {
+        final Collection<Film> users = filmController.getAllFilms();
+        assertEquals(1, users.size());
+
+        final Film film = users.stream().limit(1).collect(Collectors.toList()).get(0);
+
+        assertEquals(1, film.getId());
+        assertEquals("Film Updated", film.getName());
+        assertEquals("New film update description", film.getDescription());
+        assertEquals(LocalDate.parse("1989-04-17", FORMATTER), film.getReleaseDate());
+        assertEquals(190, film.getDuration());
+    }
 }
