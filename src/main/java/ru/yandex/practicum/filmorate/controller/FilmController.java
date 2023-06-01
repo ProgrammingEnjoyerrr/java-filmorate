7119add018808c682ac513aaba0e047ff5af58ea
Ruslan Frolov <r.frolov@1440.space>
2023-06-01 22:49:56 +0300

    refactor: remove duplicating checks

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.FilmValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,8 +18,6 @@ public class FilmController {
     @PostMapping
     @ResponseBody
     public Film create(@Valid @RequestBody Film film) {
-        validate(film);
-
         ++id;
         film.setId(id);
         idToFilm.put(id, film);
@@ -35,8 +32,6 @@ public class FilmController {
                     "с несуществующим id " + film.getId());
         }
 
-        validate(film);
-
         idToFilm.put(id, film);
         return film;
     }
@@ -44,24 +39,5 @@ public class FilmController {
     @GetMapping
     public Collection<Film> getAllFilms() {
         return idToFilm.values();
-    }
-
-    private void validate(final Film film) {
-        if (film.getName().isBlank()) {
-            throw new FilmValidationException("название не может быть пустым.");
-        }
-
-        if (film.getDescription().length() > 200) {
-            throw new FilmValidationException("максимальная длина описания — 200 символов");
-        }
-
-        final LocalDate filmBirthDay = LocalDate.of(1895, 12, 28);
-        if (film.getReleaseDate().isBefore(filmBirthDay)) {
-            throw new FilmValidationException("дата релиза — не раньше 28 декабря 1895 года");
-        }
-
-        if (film.getDuration() <= 0) {
-            throw new FilmValidationException("продолжительность фильма должна быть положительной");
-        }
     }
 }
