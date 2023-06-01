d20db13d1bddc78ce7f6357249c5511b29c0b417
Ruslan Frolov <r.frolov@1440.space>
2023-06-01 23:41:10 +0300

    fix ci: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/utils/validation/impl/MaxByteLengthValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/utils/validation/impl/MaxByteLengthValidator.java
@@ -8,9 +8,11 @@ import java.nio.charset.Charset;
 
 public class MaxByteLengthValidator implements ConstraintValidator<MaxByteLength, String> {
     private int max;
+
     public void initialize(MaxByteLength constraintAnnotation) {
         this.max = constraintAnnotation.value();
     }
+
     public boolean isValid(String object, ConstraintValidatorContext constraintContext) {
         return object == null || object.getBytes(Charset.defaultCharset()).length <= this.max;
     }
