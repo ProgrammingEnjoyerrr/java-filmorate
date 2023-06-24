6e4bb6f0987e5dd5d86a2b67c6bb2f0b40892840
Ruslan Frolov <r.frolov@1440.space>
2023-06-24 17:21:41 +0300

    feat: nice exception handling

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -2,12 +2,20 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.exception.FilmValidationException;
 import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
+import ru.yandex.practicum.filmorate.exception.UserValidationException;
 import ru.yandex.practicum.filmorate.model.ErrorResponse;
+import ru.yandex.practicum.filmorate.model.ValidationErrorResponse;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice(value = "ru.yandex.practicum.filmorate.controller")
 @Slf4j
@@ -26,6 +34,50 @@ public class ErrorHandler {
         log.error("error occurred: {}", e.getMessage(), e);
         return new ErrorResponse(e.getMessage());
     }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleUserValidationException(final UserValidationException e) {
+        log.error("error occurred: {}", e.getMessage(), e);
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleFilmValidationException(final FilmValidationException e) {
+        log.error("error occurred: {}", e.getMessage(), e);
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse handleConstraintValidationException(ConstraintViolationException e) {
+        log.error("error occurred: {}", e.getMessage(), e);
+
+        final List<ValidationErrorResponse.Violation> violations = e.getConstraintViolations()
+                .stream()
+                .map(violation -> new ValidationErrorResponse.Violation(
+                        violation.getPropertyPath().toString(),
+                        violation.getMessage())
+                )
+                .collect(Collectors.toList());
+
+        return new ValidationErrorResponse(violations);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("error occurred: {}", e.getMessage(), e);
+
+        final List<ValidationErrorResponse.Violation> violations = e.getBindingResult().getFieldErrors()
+                .stream()
+                .map(error -> new ValidationErrorResponse.Violation(error.getField(), error.getDefaultMessage()))
+                .collect(Collectors.toList());
+
+        return new ValidationErrorResponse(violations);
+    }
+
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
